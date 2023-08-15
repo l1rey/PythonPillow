fa10ac555b3ea4ff87e0739db93ed1026f2782fa
LiRey <pa.samoilova@gmail.com>
2023-08-15 11:45:00 +0300

    crop

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 from PIL import Image
 
+
+im = Image.open("pythonpillow.jpg")
+
+
 def main():
-    im = Image.open("pythonpillow.jpg")
     print(im.format, im.size, im.mode)
 
     size = (128, 128)
     outfile = "picturepython.jpg"
     im.thumbnail(size)
     im.save(outfile, "JPEG")
+
+
+def crop_image():
+    box = (110, 110, 400, 400)
+    region = im.crop(box)
+    im.save("picturepython2.jpg", "JPEG")
+
 
 if __name__ == '__main__':
     main()
