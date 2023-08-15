aa5bcbdf16ca73ccbfc2ed568582a4f0b369c419
LiRey <pa.samoilova@gmail.com>
2023-08-15 11:24:26 +0300

    thumbnail

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ from PIL import Image
 
 def main():
     im = Image.open("pythonpillow.jpg")
-    im.show()
+    print(im.format, im.size, im.mode)
 
+    size = (128, 128)
+    outfile = "picturepython.jpg"
+    im.thumbnail(size)
+    im.save(outfile, "JPEG")
 
 if __name__ == '__main__':
     main()
